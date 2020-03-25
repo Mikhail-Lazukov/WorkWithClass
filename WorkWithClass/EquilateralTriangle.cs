8c234597c2293a23e8c43c8750cf3f28f1509bc9
misha <misha130200@gmail.com>
2020-03-26 00:36:38 +0700

    Задание выполнено

---
+++ WorkWithClass/EquilateralTriangle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class EqTriangle
+{
+    public double A { get; set; }
+    public double B { get; set; }
+    public double C { get; set; }
+
+    public EqTriangle() { }
+
+    public EqTriangle( double a, double b, double c)
+	{
+        A = a;
+        B = b;
+        C = c;
+	}
+
+    public EqTriangle (EqTriangle anotherTriangle)
+    {
+        A = anotherTriangle.A;
+        B = anotherTriangle.B;
+        C = anotherTriangle.C;
+    }
+
+    public bool DoesThisTriangleExist()
+    {
+        if (A > 0 && B > 0 && C > 0 && A == B && A == C)
+            return true;
+        else
+            return false;
+    }
+
+    public double Perimeter ()
+    {
+        if (DoesThisTriangleExist())
+            return A + B + C;
+        else
+            return double.NaN;
+    }
+
+    public double Square()
+    {
+        if (DoesThisTriangleExist())
+            return A * A * Math.Sqrt(3) / 4;
+        else
+            return double.NaN;
+    }
+
+}
--- WorkWithClass/Program.cs
+++ WorkWithClass/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WorkWithClass
 {
@@ -10,6 +6,33 @@ namespace WorkWithClass
     {
         static void Main(string[] args)
         {
+            EqTriangle firstTriangle = new EqTriangle();
+            EqTriangle secondTriangle = new EqTriangle(5.5, 3.4, 4.5);
+            EqTriangle thirdTriangle = new EqTriangle(3.6, 3.6, 3.6);
+            EqTriangle fourthTriangle = new EqTriangle(thirdTriangle);
+
+            Console.WriteLine("Является ли треугольник равносторонним:");
+            Console.WriteLine("Первый треугольник: " + firstTriangle.DoesThisTriangleExist().ToString());
+            Console.WriteLine("Второй треугольник: " + secondTriangle.DoesThisTriangleExist().ToString());
+            Console.WriteLine("Третий треугольник: " + thirdTriangle.DoesThisTriangleExist().ToString());
+            Console.WriteLine("Четвертый треугольник: " + fourthTriangle.DoesThisTriangleExist().ToString());
+            Console.WriteLine("\n-------------------------------------------\n");
+
+            Console.WriteLine("Вычисление периметра:");
+            Console.WriteLine("Первый треугольник: " + firstTriangle.Perimeter().ToString());
+            Console.WriteLine("Второй треугольник: " + secondTriangle.Perimeter().ToString());
+            Console.WriteLine("Третий треугольник: " + thirdTriangle.Perimeter().ToString());
+            Console.WriteLine("Четвертый треугольник: " + fourthTriangle.Perimeter().ToString());
+            Console.WriteLine("\n-------------------------------------------\n");
+
+            Console.WriteLine("Вычисление площади:");
+            Console.WriteLine("Первый треугольник: " + firstTriangle.Square().ToString());
+            Console.WriteLine("Второй треугольник: " + secondTriangle.Square().ToString());
+            Console.WriteLine("Третий треугольник: " + thirdTriangle.Square().ToString());
+            Console.WriteLine("Четвертый треугольник: " + fourthTriangle.Square().ToString());
+            Console.WriteLine("\n-------------------------------------------\n");
+
+            Console.ReadLine();
         }
     }
 }
